0631c8698b2098ad8131cb467fdff4499bc031f1
Renato Lima <natenho@gmail.com>
2020-11-29 01:34:27 -0300

    Refactor and add initial support for multiple heatmap palettes using Chroma.js

--- heatmap.js
+++ heatmap.js
@@ -5,47 +5,45 @@ chrome.runtime.onMessage.addListener((request, _sender, _response) => {
   console.timeEnd(request.menuFunction);
 });
 
-function clearHeatMap() {  
+function clearHeatMap() {
   for (var index = 0; index < elements.length; index++) {
     elements[index].parentElement.style = "";
   }
 }
 
-function tableHeatMap() {  
-  for (let index = 0; index <= elements.length; index++) {
-    var elementsToBeColorized = activeGwtFrame.querySelectorAll(
-      `tbody tr [class='data'] [rel]`
-    );
-    var cellValues = extractValues(elementsToBeColorized);
-    var palette = buildPalette(cellValues);
-    colorize(elementsToBeColorized, cellValues, palette);
-  }
+function tableHeatMap() {
+  var elementsToBeColorized = activeGwtFrame.querySelectorAll(
+    `tbody tr [class='data'] [rel]`
+  );
+  heatMap(elementsToBeColorized);
 }
 
-function colHeatMap() {  
+function colHeatMap() {
   for (let col = 0; col <= lastRel.col; col++) {
     var elementsToBeColorized = activeGwtFrame.querySelectorAll(
       `tbody tr [class='data'] [rel^='${col}:']`
     );
-    var cellValues = extractValues(elementsToBeColorized);
-    var palette = buildPalette(cellValues);
-    colorize(elementsToBeColorized, cellValues, palette);
+    heatMap(elementsToBeColorized);
   }
 }
 
-function rowHeatMap() {  
+function rowHeatMap() {
   for (let row = 0; row <= lastRel.row; row++) {
     var elementsToBeColorized = activeGwtFrame.querySelectorAll(
       `tbody tr [class='data'] [rel$=':${row}']`
     );
-    var cellValues = extractValues(elementsToBeColorized);
-    var palette = buildPalette(cellValues);
-    colorize(elementsToBeColorized, cellValues, palette);
+    heatMap(elementsToBeColorized);
   }
+}
+
+function heatMap(elementsToBeColorized) {
+  var cellValues = extractValues(elementsToBeColorized);
+  colorize(elementsToBeColorized, cellValues);
 }
 
 // To export colored excel file: https://github.com/SheetJS/sheetjs/issues/1795
 
+// Select all data elements in the pivot (elements with the class "data" containing a "rel" attribute)
 function fetchDataElements() {
   activeGwtFrame = document.querySelector(
     'div[style="width: 100%; height: 100%; padding: 0px; margin: 0px; position: relative; left: 0px;"] .gwt-Frame'
@@ -58,6 +56,7 @@ function fetchDataElements() {
   lastRel = getLastRel(elements);
 }
 
+// "rel" attribute contains the cell coordinate in pivot in the format "col:row"
 function getLastRel(elements) {
   var lastElement = elements[elements.length - 1];
   lastElement.getAttribute("rel");
@@ -65,6 +64,7 @@ function getLastRel(elements) {
   return { col: lastElementRel[0], row: lastElementRel[1] };
 }
 
+// Generate an array of the values contained in the "alt" attribute of the elements
 function extractValues(elements) {
   var cellValues = new Array(length);
 
@@ -76,41 +76,26 @@ function extractValues(elements) {
   return cellValues;
 }
 
-var minValue = (a, b) => (isNaN(b) || b > a ? a : b);
-var maxValue = (a, b) => (isNaN(b) || b < a ? a : b);
-var nearestValue = (arr, val) =>
-  arr.reduce(
-    (p, n) => (Math.abs(p) > Math.abs(n - val) ? n - val : p),
-    Infinity
-  ) + val;
+var normalize = (v, min, max) => (v - min) / (max - min);
+var minValueIgnoringNull = (a, b) => (isNaN(b) || b > a ? a : b);
+var maxValueIgnoringNull = (a, b) => (isNaN(b) || b < a ? a : b);
 
-function colorize(elements, cellValues, palette) {
+// Change the element style based on the relative index in the pallette
+function colorize(elements, values) {
+  const PALETTE_SIZE = 255;
+
+  var colors = chroma
+    .scale(["lightgreen", "yellow", "red"])
+    .mode("lab")
+    .colors(PALETTE_SIZE);
+
+  var min = values.reduce(minValueIgnoringNull, Number.MAX_VALUE);
+  var max = values.reduce(maxValueIgnoringNull, Number.MIN_VALUE);
+
   for (var index = 0; index < elements.length; index++) {
-    var near = nearestValue(palette, cellValues[index]);
-
-    var colorIndex = 255 - palette.indexOf(near);
-
+    var colorIndex = Math.round(normalize(values[index], min, max) * (PALETTE_SIZE - 1));
     elements[
       index
-    ].parentElement.style = `background-color: rgb(255, ${colorIndex}, ${colorIndex}) !important`;
+    ].parentElement.style = `background-color: ${colors[colorIndex]} !important`;
   }
-}
-
-function buildPalette(values) {
-  var min = values.reduce(minValue, Number.MAX_VALUE);
-  var max = values.reduce(maxValue, Number.MIN_VALUE);
-
-  var difference = max - min;
-  var bucketSize = Math.round(difference / 255);
-
-  var palette = new Array(255);
-
-  var index = 0,
-    length = palette.length;
-
-  for (; index < length; index++) {
-    palette[index] = min + (index + 1) * bucketSize;
-  }
-
-  return palette;
 }
